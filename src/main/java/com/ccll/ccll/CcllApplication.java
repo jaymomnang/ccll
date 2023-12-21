95fa469af34f441dbd493bebda4a1baba95747fc
Jay Momnang <juliusmomnang@gmail.com>
2023-12-21 12:04:55 +0100

    initialize

---
+++ src/main/java/com/ccll/ccll/CcllApplication.java
@@ -0,0 +1,13 @@
+package com.ccll.ccll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccll/ccll/CcllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccll.ccll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
