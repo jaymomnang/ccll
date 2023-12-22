7df328f054f923173ada977a15d22d3871caaa56
Jay Momnang <juliusmomnang@gmail.com>
2023-12-22 05:24:48 +0100

    initialized

---
+++ src/main/java/com/ccl/learning/LearningApplication.java
@@ -0,0 +1,18 @@
+package com.ccl.learning;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(LearningApplication.class);
+		app.setDefaultProperties(Collections
+          .singletonMap("server.port", "7000"));
+        app.run(args);
+	}
+
+}
