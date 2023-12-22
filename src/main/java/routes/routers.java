24d2609171af0da3d49c3330abe7c1b63ed675b2
Jay Momnang <juliusmomnang@gmail.com>
2023-12-22 06:30:29 +0100

    routers removed

--- src/main/java/routes/routers.java
+++ src/main/java/routes/routers.java
@@ -2,9 +2,11 @@ package routes;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import org.springframework.web.*;
+
+//import org.springframework.web.cors.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import repos.*;
 import models.*;
 import controllers.*;
